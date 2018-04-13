57c3da0578d077a1f3a1aa54b239cc5b13af7da2
Nenad <nesov97@gmail.com>
2018-04-13 21:04:23 +0200

    popravljen poziv procedure returnSumOfSalary u FacultyRepositoryImpl

--- faculty/src/main/java/ba/telegroup/apps/faculty/repository/repositoryCustom/repositoryImpl/FacultyRepositoryImpl.java
+++ faculty/src/main/java/ba/telegroup/apps/faculty/repository/repositoryCustom/repositoryImpl/FacultyRepositoryImpl.java
@@ -2,15 +2,12 @@ package ba.telegroup.apps.faculty.repository.repositoryCustom.repositoryImpl;
 
 import ba.telegroup.apps.faculty.repository.repositoryCustom.FacultyRepositoryCustom;
 
-import javax.persistence.EntityManager;
-import javax.persistence.ParameterMode;
-import javax.persistence.PersistenceContext;
+import javax.persistence.*;
 import javax.persistence.StoredProcedureQuery;
 
 public class FacultyRepositoryImpl implements FacultyRepositoryCustom {
 
     @PersistenceContext
-    private
     EntityManager entityManager;
 
     @Override
@@ -18,9 +15,12 @@ public class FacultyRepositoryImpl implements FacultyRepositoryCustom {
         StoredProcedureQuery query = entityManager.createStoredProcedureQuery("returnSumOfSalary");
         query.registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN).setParameter(1, facultyId);
         query.registerStoredProcedureParameter(2, Double.class, ParameterMode.OUT);
-        if (query.execute())
-            return (Double) query.getOutputParameterValue(2);
-        return null;
+//        The "false" returned by "query.execute()" means that the JDBC statement didn't read any rows.
+//        This is expected, as stored procedures don't directly return ResultSets, they only return values for OUT parameters.
+//        if (query.execute())
+//            return (Double) query.getOutputParameterValue(2);
+//        return null;
+        return (Double) query.getOutputParameterValue(2);
     }
 
 }
